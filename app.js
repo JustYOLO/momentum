55f75315237062e4f318c6e75ab2f4613331702b
Just_YOLO <nascarf16@gmail.com>
2022-12-26 12:25:24 +0900

    Javascript day 2

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log(23238583528)
+const age = parseInt(prompt("How old are you"));
+
+if(isNaN(age) || age < 0) {
+    console.log("Please write a real number");
+} else if(age < 18) {
+    console.log("You can't buy alcohol");
+} else if(age >= 18 && age <= 50) {
+    console.log("You can drink alcohol");
+} else if(age === 100) {
+    console.log("wow you're wise");
+} else if(age > 80) {
+    console.log("You can do whatever you want");
+}
+
+
+else {
+    console.log("You should exercise");
+}
