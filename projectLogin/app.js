aff0367b1ea2cec57d30ee27eccad9487ce1146b
Just_YOLO <nascarf16@gmail.com>
2022-12-29 23:46:24 +0900

    Maked login form (seems not good)

--- projectLogin/app.js
+++ projectLogin/app.js
@@ -2,6 +2,5 @@ const htmlElement = document.querySelector("#html");
 const anchorElement = document.querySelector("#title");
 const emptyElement = document.querySelector("#sadfowanv");
 
-anchorElement.style.color = "blue";
 console.log(htmlElement.style);
 console.log(styleSheet);
