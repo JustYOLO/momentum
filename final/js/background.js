29499efae2055efe67b836415a8bdf350b161c65
Just_YOLO <nascarf16@gmail.com>
2023-01-07 11:12:06 +0900

    Before using pico css

---
+++ final/js/background.js
@@ -0,0 +1,5 @@
+html = document.querySelector("html");
+const images = ["1.jpg", "2.jpg", "3.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+html.style.background = `no-repeat center / auto 100% url("background/${chosenImage}")`;
