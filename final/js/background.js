27673b3ffad931fbfcbe1d8126051c92b163352f
Just_YOLO <nascarf16@gmail.com>
2023-01-07 17:04:20 +0900

    Chat GPT css

--- final/js/background.js
+++ final/js/background.js
@@ -2,4 +2,4 @@ html = document.querySelector("html");
 const images = ["1.jpg", "2.jpg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-html.style.background = `no-repeat center / auto 100% url("background/${chosenImage}")`;
+html.style.background = `no-repeat center / auto 110% url("background/${chosenImage}")`;
